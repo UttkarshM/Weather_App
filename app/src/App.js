04db31446c2dd4974e4890737fdfca3b1de18d76
UttkarshM <uttkarshm0404@gmail.com>
2024-09-20 18:18:45 +0530

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+import Weather from './components/Weather';
+function App() {
+  return (
+    <div className="flex w-full h-[100vh]">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Weather.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import '../input.css'
+
+const Weather = () => {
+    return (
+    <>
+    <h1>h</h1>
+    <div className='w-[50%] h-[50%] bg-orange-400'>
+        fsd
+    </div>
+    </>  
+    );
+  }
+  
+export default Weather;
